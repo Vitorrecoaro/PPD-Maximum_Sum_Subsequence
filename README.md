e21cbcb329a61da4d51d56bfc38d40f3ae3844f3
Vitor Recoaro <vitordearec@gmail.com>
2023-01-31 00:24:25 -0300

    Arquivos iniciais

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# PPD-Maximum_Sum_Subsequence
-Repository for distributed and parallel programming class for maximum sum subsequence problem from marathon of parallel programming
+# UFSCar - PPD - Maximum sum subsequence
+
+## Summary
+
+Repository for distributed and parallel programming class for maximum sum subsequence problem from 14th marathon of parallel programming. [Available in this link](http://lspd.mackenzie.br/marathon/19/problems.html)
+
+Teacher:
+
+-   Helio Crestana Guardia
+
+Students:
+
+-   Bruno Rodrigues Medri
+-   Bruno Leandro Pereira
+-   Thiago Roberto Garcia Albino
+-   Vitor de Almeida Recoaro
---
+++ sequencial.c
@@ -0,0 +1,52 @@
+
+#include<stdio.h>
+#include<stdlib.h> 
+#define MAX(a,b) a > b ? a : b
+
+int MaxIncreasingSub(int *arr, int n, int k) 
+{ 
+	int **dp, ans = -1;
+	dp = malloc(n * sizeof(int*));
+	for(int i=0; i < n; i++)
+		dp[i] = malloc((k+1) * sizeof(int));
+	for(int i = 0; i < n; i++){
+		for(int j = 0; j < k; j++){
+			dp[i][j] = -1;
+		}
+	}
+	for (int i = 0; i < n; i++) { 
+		dp[i][1] = arr[i]; 
+	} 
+	for (int i = 1; i < n; i++) { 
+		for (int j = 0; j < i; j++) { 
+			if (arr[j] < arr[i]) { 
+				for (int l = 1; l <= k - 1; l++) { 
+					if (dp[j][l] != -1) { 
+						dp[i][l + 1] = MAX(dp[i][l + 1],dp[j][l] + arr[i]); 
+					} 
+				} 
+			} 
+		} 
+	} 
+
+	for (int i = 0; i < n; i++) { 
+		if (ans < dp[i][k]) 
+			ans = dp[i][k]; 
+	} 
+
+	return (ans == -1) ? 0 : ans; 
+} 
+
+int main() 
+{ 
+	int n, k;
+    scanf(" %d", &n);
+    scanf(" %d", &k);
+    int *arr = malloc(n * sizeof(int));
+	for (int i=0; i<n; i++)
+		scanf(" %d", &arr[i]);
+	int ans = MaxIncreasingSub(arr, n, k); 
+	printf("%d\n", ans); 
+	return 0;
+} 
+
