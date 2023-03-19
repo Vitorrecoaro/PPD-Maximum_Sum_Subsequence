29c5cac7e1f8a1e4979f13cdc7a50c5a2e87eb77
Vitor Recoaro <vitordearec@gmail.com>
2023-03-19 12:19:39 -0300

    add: Define answer with CUDA

--- cuda-prog.cu
+++ cuda-prog.cu
@@ -17,10 +17,8 @@ __global__ void calculatePartialSum( int totalOperations, int col, int k, int *a
     int idx = threadIdx.x + blockIdx.x * blockDim.x;
 
     if(idx < totalOperations){
-        int refValue = (int)(1 + sqrtf((float)(1-4*(col - (col * col) - 2 * idx))))/2;
-        int compValue = (int)(idx) - (int)(refValue * refValue + col - refValue - col * col)/2;
-
-        // printf("idx: %d, ref: %d, cmp: %d\n", idx, refValue, compValue);
+        int refValue = (int)(1 + sqrtf((float)(1 - 4 * (col - (col * col) - 2 * idx)))) / 2;
+        int compValue = (int)(idx) - (int)(refValue * refValue + col - refValue - col * col) / 2;
 
         if(arr[compValue] < arr[refValue]){
             if(dp[compValue * (k + 1) + col] != -1){
@@ -31,53 +29,60 @@ __global__ void calculatePartialSum( int totalOperations, int col, int k, int *a
     }
 }
 
+__global__ void calculateAnswer(int n, int k, int *ans, int *dp){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+
+    if (idx < n){
+        if (*ans < dp[ idx * (k + 1) + k]){
+            atomicMax(ans, dp[ idx * (k + 1) + k]);
+        }
+    }
+}
+
 int MaxIncreasingSub(int *arr, int n, int k) 
 {
-	int *dp = (int*)malloc(n * (k+1) * sizeof(int));
+	int *dp = (int *)malloc(n * (k + 1) * sizeof(int));
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < k+1; j++) {
-            dp[i * (k+1) + j] = -1;
+        for (int j = 0; j < k + 1; j++) {
+            dp[i * (k + 1) + j] = -1;
         }
     }
 
     for (int i = 0; i < n; i++) {
-        dp[i * (k+1) + 1] = arr[i];
+        dp[i * (k + 1) + 1] = arr[i];
     }
 
     int *d_arr, *d_dp;
+
     cudaMalloc((void **)&d_arr, n * sizeof(int));
-    cudaMalloc((void **)&d_dp, n * (k+1) * sizeof(int));
+    cudaMalloc((void **)&d_dp, n * (k + 1) * sizeof(int));
     cudaMemcpy(d_arr, arr, n * sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_dp, dp, n * (k+1) * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_dp, dp, n * (k + 1) * sizeof(int), cudaMemcpyHostToDevice);
+    free(dp);
 
 	for(int i = 0; i < k; i ++){
         int threadsPerBlock = 1024;
-        int totalOperations = (n*n + i - (n + i*i))/2;
+        int totalOperations = (n * n + i - (n + i * i)) / 2;
         int blocksPerGrid = (totalOperations + threadsPerBlock - 1) / threadsPerBlock;
         calculatePartialSum<<<blocksPerGrid, threadsPerBlock>>>( totalOperations, i, k, d_arr, d_dp);
         cudaDeviceSynchronize();
 	}
 
-    cudaMemcpy(dp, d_dp, n * (k+1) * sizeof(int), cudaMemcpyDeviceToHost);
+    int ans, *d_ans;
 
-    // for(int i = 0; i < n; i++){
-    //     for(int j = 0; j < k + 1; j++){
-    //         printf("%d ", dp[i * (k + 1) + j]);
-    //     }
-    //     printf("\n");
-    // }
-    
+    ans = -1;
+    cudaMalloc((void **)&d_ans, sizeof(int));
+    cudaMemcpy(d_ans, &ans, sizeof(int), cudaMemcpyHostToDevice);
+
+    int threadPerBlock = 1024;
+    int blockPerGrid = ( n + threadPerBlock - 1 ) / threadPerBlock;
+    calculateAnswer<<<blockPerGrid, threadPerBlock>>>(n, k, d_ans, d_dp);
+    cudaDeviceSynchronize();
+    cudaMemcpy(&ans, d_ans, sizeof(int), cudaMemcpyDeviceToHost);
     cudaFree(d_arr);
     cudaFree(d_dp);
+    cudaFree(d_ans);
 
-    int ans = -1;
-    for (int i = 0; i < n; i++) {
-        if (ans < dp[i * (k+1) + k]) {
-            ans = dp[i * (k+1) + k];
-        }
-    }
-
-    free(dp);
     return (ans == -1) ? 0 : ans; 
 } 
 
